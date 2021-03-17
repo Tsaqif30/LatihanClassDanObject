f105fb8a1c8c33b55c0e515a196a80fb92bb99b3
oca <jxo.uk30@gmail.com>
2021-03-17 16:56:01 +0700

    latihanclassdanobject

---
+++ tugas2/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas2
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method 
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty) Console.WriteLine("On Duty : Yes");
+            else Console.WriteLine("On Duty : No ");
+            Console.WriteLine("Number of Passanger : {0}", NumPassenger);
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
